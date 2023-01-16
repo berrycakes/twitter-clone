d5ffb901a00a7a3d5eea7bfc19d66ae2c94f36e9
Jianne Gabriel <jianne@advance.ph>
2023-01-16 18:36:46 +0800

    Display form provider as grid

--- components/form/FormProvider.tsx
+++ components/form/FormProvider.tsx
@@ -1,25 +1,22 @@
-import { FormProvider as Form, UseFormReturn } from 'react-hook-form'
+import clsx from 'clsx';
+import { FormProvider as Form, UseFormReturn } from 'react-hook-form';
+import styles from './FormProvider.module.css';
 
 type FormProviderProps = {
-  children: React.ReactNode
-  methods: UseFormReturn<any>
-  onSubmit?: VoidFunction
-  className?: string
-}
+  children: React.ReactNode;
+  methods: UseFormReturn<any>;
+  onSubmit?: VoidFunction;
+  className?: string;
+};
 
-const FormProvider = ({
-  children,
-  onSubmit,
-  className,
-  methods,
-}: FormProviderProps) => {
+const FormProvider = ({ children, onSubmit, className, methods }: FormProviderProps) => {
   return (
     <Form {...methods}>
-      <form onSubmit={onSubmit} className={className}>
+      <form onSubmit={onSubmit} className={clsx(className, styles.form)}>
         {children}
       </form>
     </Form>
-  )
-}
+  );
+};
 
-export default FormProvider
+export default FormProvider;
