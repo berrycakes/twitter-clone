1b09c4a4b70069a7599b365aabe4c51eb04b9ca3
Jianne Gabriel <jianne@advance.ph>
2023-01-16 12:13:13 +0800

    Update button style

--- components/ui-kit/Button.tsx
+++ components/ui-kit/Button.tsx
@@ -1,13 +1,25 @@
+import clsx from 'clsx';
 import { ButtonHTMLAttributes, ReactNode } from 'react';
 import styles from './Button.module.css';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
+  variant?: 'primary' | 'secondary';
+  fullWidth?: boolean;
 }
 
-const Button = ({ children, ...other }: ButtonProps) => {
+const Button = ({ children, variant, disabled, fullWidth = true, ...other }: ButtonProps) => {
+  const variantStyle = variant === 'secondary' ? styles.secondary : styles.primary;
   return (
-    <button className={styles.button} {...other}>
+    <button
+      className={clsx(
+        variantStyle,
+        disabled && styles.disabled,
+        fullWidth && styles.fullWidth,
+        styles.button
+      )}
+      {...other}
+    >
       {children}
     </button>
   );
