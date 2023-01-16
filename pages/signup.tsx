ed8ea77f820332a80ac6c632cc9359580fc5983f
Jianne Gabriel <jianne@advance.ph>
2023-01-16 15:58:23 +0800

    Use card compoennt for signup page

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,6 +1,8 @@
 import { useUser } from '@supabase/auth-helpers-react';
+import clsx from 'clsx';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import Card from '../components/ui-kit/Card';
 import { PATH } from '../routes/paths';
 import AuthSignUp from '../sections/AuthSignUp';
 import styles from '../styles/Home.module.css';
@@ -19,10 +21,12 @@ const SignUp = () => {
         <title>Sign Up | Twitter Clone</title>
       </Head>
       <main className={styles.main}>
-        <h1>Sign Up</h1>
-        <div>
-          <AuthSignUp />
-        </div>
+        <Card className={styles.signUp}>
+          <h4>Create an account</h4>
+          <div>
+            <AuthSignUp />
+          </div>
+        </Card>
       </main>
     </>
   );
