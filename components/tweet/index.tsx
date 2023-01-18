5cf8f9ea824d459906d9003034c13324d292d1fe
Jianne Gabriel <jianne@advance.ph>
2023-01-18 21:42:19 +0800

    Disable tweet button when content is empty or exceeding char limit

--- components/tweet/index.tsx
+++ components/tweet/index.tsx
@@ -1,20 +1,52 @@
 import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import updateLocale from 'dayjs/plugin/updateLocale';
 import Card from '../ui-kit/Card';
 import Footer from './footer';
 import Header from './header';
 import styles from './styles.module.css';
 
+dayjs.extend(relativeTime);
+dayjs.extend(updateLocale);
+
+dayjs.updateLocale('en', {
+  relativeTime: {
+    future: 'in %s',
+    past: '%s ago',
+    s: 'now',
+    m: '1m',
+    mm: '%dm',
+    h: '1h',
+    hh: '%dh',
+    d: '1d',
+    dd: 'MMM DD',
+    y: 'MMM DD YY',
+  },
+});
+
 type TweetProps = {
   content: string;
   username: string;
+  displayName: string;
   date: Date;
 };
 
 const Tweet = (props: TweetProps) => {
-  const { username, content, date } = props;
+  const { username, displayName, content, date } = props;
+  const displayDate =
+    dayjs(new Date()).diff(date, 'hours') < 23
+      ? dayjs(date).fromNow(true)
+      : dayjs(date).year() === dayjs().year()
+      ? dayjs(date).format('MMM DD')
+      : dayjs(date).format('MMM DD YY');
   return (
     <Card className={styles.container} padding="1.5rem">
-      <Header name={username} date={dayjs(date).format('MMM D')} createMode={false} />
+      <Header
+        name={username}
+        displayName={displayName}
+        date={displayDate}
+        createMode={false}
+      />
       <div className={styles.content}>{content}</div>
       <Footer />
     </Card>
--- components/tweet/tweet-button-group/index.tsx
+++ components/tweet/tweet-button-group/index.tsx
@@ -1,3 +1,4 @@
+import { ButtonHTMLAttributes } from 'react';
 import {
   MdCalendarToday,
   MdEmojiEmotions,
@@ -10,17 +11,51 @@ import Button from '../../ui-kit/Button';
 import IconButton from '../../ui-kit/IconButton';
 import styles from './styles.module.css';
 
-const TweetButtonGroup = () => {
+type TweetButtonGroupProps = {
+  tweetButtonProps?: ButtonHTMLAttributes<HTMLButtonElement>;
+};
+
+const TweetButtonGroup = ({ tweetButtonProps }: TweetButtonGroupProps) => {
   return (
     <div className={styles.container}>
-      <IconButton type="button" width={40} height={40} icon={<MdInsertPhoto size={24} />} />
-      <IconButton type="button" width={40} height={40} icon={<MdGif size={24} />} />
-      <IconButton type="button" width={40} height={40} icon={<MdInsertChart size={24} />} />
-      <IconButton type="button" width={40} height={40} icon={<MdEmojiEmotions size={24} />} />
-      <IconButton type="button" width={40} height={40} icon={<MdCalendarToday size={24} />} />
-      <IconButton type="button" width={40} height={40} icon={<MdLocationOn size={24} />} />
+      <IconButton
+        type="button"
+        width={40}
+        height={40}
+        icon={<MdInsertPhoto size={24} />}
+      />
+      <IconButton
+        type="button"
+        width={40}
+        height={40}
+        icon={<MdGif size={24} />}
+      />
+      <IconButton
+        type="button"
+        width={40}
+        height={40}
+        icon={<MdInsertChart size={24} />}
+      />
+      <IconButton
+        type="button"
+        width={40}
+        height={40}
+        icon={<MdEmojiEmotions size={24} />}
+      />
+      <IconButton
+        type="button"
+        width={40}
+        height={40}
+        icon={<MdCalendarToday size={24} />}
+      />
+      <IconButton
+        type="button"
+        width={40}
+        height={40}
+        icon={<MdLocationOn size={24} />}
+      />
       <div className={styles.buttonContainer}>
-        <Button type="submit" fullWidth>
+        <Button type="submit" fullWidth {...tweetButtonProps}>
           Tweet
         </Button>
       </div>
